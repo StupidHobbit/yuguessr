de2f2e7053b9cb5dfa57e565c22637f72f2fd450
StupidHobbit <3mir33@gmail.com>
2019-12-07 16:26:51 +0300

    Configured build and run

--- server/make_app.py
+++ server/make_app.py
@@ -1,3 +1,5 @@
+from os import path
+
 import falcon
 
 from server.resources import main_page, game, start_game
@@ -5,7 +7,10 @@ from server.resources import main_page, game, start_game
 
 def make_app():
     app = falcon.API()
-    app.add_route('/', main_page.Resource)
-    app.add_route('/game/{id}', game.Resource)
-    app.add_route('/start_game', start_game.Resource)
+
+    app.add_static_route('/', path.abspath('build'))
+
+    app.add_route('/', main_page.Resource())
+    app.add_route('/game/{id}', game.Resource())
+    app.add_route('/start_game', start_game.Resource())
     return app
--- server/resources/main_page.py
+++ server/resources/main_page.py
@@ -1,8 +1,18 @@
+import os
+
 import falcon
 
 from server.resources.base import BaseResource
 
 
 class Resource(BaseResource):
+    def __init__(self):
+        path = 'build/index.html'
+        stream = open(path, 'rb')
+        self.content_length = os.path.getsize(path)
+        self.body = stream.read()
+
     def on_get(self, request: falcon.Request, response: falcon.Response):
-        pass
+        response.body = self.body
+        response.content_length = self.content_length
+        response.content_type = 'text/html'
