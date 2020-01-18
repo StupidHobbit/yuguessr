bf46aad6a2ddfd10b5b6c99d0195f89def5dc1f9
TomSssM <ilyashome3@gmail.com>
2020-01-18 14:36:51 +0300

    edit condifs

--- client/components/Landing/Landing.js
+++ client/components/Landing/Landing.js
@@ -5,14 +5,20 @@ import { Link } from 'react-router-dom';
 import './Landing.scss';
 
 class Landing extends Component {
+    startGame = () => {
+        console.log('satrt game');
+    };
+
     render() {
         return (
             <main className="landing">
                 <FontAwesomeIcon className="landing__icon" icon={faGlobe} />
-                <Link to="/game">
-                    <button className="landing__start-game-button">
-                        Start Game
-                    </button>
+                <Link
+                    onClick={this.startGame}
+                    className="landing__start-game-button"
+                    to="/game"
+                >
+                    Start Game
                 </Link>
             </main>
         );
